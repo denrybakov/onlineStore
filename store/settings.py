83ddd81594789d850e0a5e26c1a7c94d78fc1188
denrybakov <rybakovdev@yandex.ru>
2023-01-02 01:40:51 +0300

    create User model

--- store/settings.py
+++ store/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'products',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -128,6 +129,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Users 
+
+AUTH_USER_MODEL = 'users.User'
