80563a955dc98d7561bd27ff7c852f62e2aa3453
denrybakov <rybakovdev@yandex.ru>
2023-01-03 01:09:42 +0300

    django contrib auth: login, registration

--- store/urls.py
+++ store/urls.py
@@ -18,6 +18,7 @@ from products.views import index
 urlpatterns = [
     path('', index, name='index'),
     path('products/', include('products.urls', namespace='products')),
+    path('users/', include('users.urls', namespace='users')),
     path('admin/', admin.site.urls),
 ]
 
