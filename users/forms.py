147974421b55f399bbab568a3fc45ad8c87671b5
denrybakov <rybakovdev@yandex.ru>
2023-01-03 02:05:52 +0300

    controller profile

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from users.models import User
 
 class UserLoginForm(AuthenticationForm):
@@ -50,7 +50,35 @@ class UserRegistrationForm(UserCreationForm):
     'class': 'form-control py-4',
     'placeholder': 'Подтвердите пароль'
   }))
-  
+
   class Meta:
     model = User 
     fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+class UserProfileForm(UserChangeForm):
+
+  first_name = forms.CharField(widget=forms.TextInput(attrs={
+    'class': 'form-control py-4'
+  }))
+
+  last_name = forms.CharField(widget=forms.TextInput(attrs={
+    'class': 'form-control py-4'
+  }))
+
+  image = forms.ImageField(widget=forms.FileInput(attrs={
+    'class': 'custom-file-input'
+  }), required=False)
+
+  username = forms.CharField(widget=forms.TextInput(attrs={
+    'class': 'form-control py-4',
+    'readonly': True
+  }))
+
+  email = forms.CharField(widget=forms.EmailInput(attrs={
+    'class': 'form-control py-4',
+    'readonly': True
+  }))
+
+  class Meta:
+    model = User 
+    fields = ('first_name', 'last_name', 'image', 'username', 'email')
--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
 from users.models import User 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 # Create views
 
@@ -37,7 +37,15 @@ def registration(request):
   return render(request, 'users/register.html', context)
 
 def profile(request):
+  if request.method == 'POST':
+    form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+    if form.is_valid():
+      form.save()
+      return HttpResponseRedirect(reverse('users:profile'))
+  else:
+    form = UserProfileForm(instance=request.user)
   context = {
-    'title': 'Store - Профиль' 
+    'title': 'Store - Профиль',
+    'form': form
   }
   return render(request, 'users/profile.html', context)
